bdcd7e1d950c42fde80288ddbcc648c6dd5a3018
Gutemberg S. Mendoza <gutembergmendoza@Gutembergs-MacBook-Pro.local>
2022-06-03 20:23:48 -0500

    Code reafactor

--- main.py
+++ main.py
@@ -6,109 +6,64 @@ import time
 import requests
 import PyPDF2
 import os
+from bs4 import BeautifulSoup
 from pdf_mail import sendpdf
 from sharepoint import SharePoint
 from datetime import date
+import datetime
 
 
-def get_sumary(text:str)->None:
+
+def get_petro_link():
+    link = "https://www.eppetroecuador.ec/?p=3721" 
+    data = requests.get(link).content
+    soup = BeautifulSoup(data, "html.parser")
+    foo = soup.find('a',string = 'Sumario de Operaciones').get('href')
+    return foo 
+
+def get_summary(text:str)->None:
     """Descarga el sumario de operaciones de petroecuador
 
     Args:
         text (_str_): Promt indicando la descarga
     """
-    link = 'https://www.eppetroecuador.ec/wp-content/uploads/downloads/2022/05/PRD-PEC-RPR-SUMARIO-OPERACIONES.pdf'
-    day = date.today().day
-    document_day = day - 1
-    link = f'https://www.eppetroecuador.ec/wp-content/uploads/downloads/2022/05/PRD_PEC-RPR-202205{document_day}-SUMARIO-OPERACIONES.pdf'
-    #
+   
+    link = get_petro_link()
+    print(link)
     data = requests.get(link).content
     print(text)
     with open('docs/sumario.pdf','wb') as file:
         file.write(data)
-    #upload_to_sharepoint()
+
     
-        
-
-def update_name():
-    pdfObj = open('docs/sumario.pdf', 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfObj)
-    pdfPage = pdfReader.getPage(0)
-    text = pdfPage.extractText()
-    date_index = text.find('Fecha de Impresión:')
-    text_date = text[date_index+20:65]
-
-    month = text_date[0:3]
-    day = text_date[4:6]
-    year = text_date[8:12]
-    hour = text_date[18]
-    
-    time_set = text_date[26:28]
-
-    time_stamped = month+'-'+day+'-'+year+'-'+hour+time_set
-    print(time_stamped)
-
-    formatted_command = 'mv docs/sumario.pdf ' + ' docs/'+time_stamped + '.pdf'
-    os.system(formatted_command)
+def month_name():
+    datetime_object = datetime.datetime.strptime(str(date.today().month), "%m")
+    full_month_name = datetime_object.strftime("%B")
+    return full_month_name
     
 
-def get_name():
-    pdfObj = open('docs/sumario.pdf', 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfObj)
-    pdfPage = pdfReader.getPage(0)
-    text = pdfPage.extractText()
-    date_index = text.find('Fecha de Impresión:')
-    text_date = text[date_index+20:65]
-
-    month = text_date[0:3]
-    day = text_date[4:6]
-    year = text_date[8:12]
-    hour = text_date[18]
-    
-    time_set = text_date[26:28]
-
-    time_stamped = month+'-'+day+'-'+year+'-'+hour+time_set
-    print(time_stamped)
-
-   # formatted_command = 'mv docs/sumario.pdf ' + ' docs/'+time_stamped + '.pdf'
-    return time_stamped   
-
-
-def upload_to_sharepoint(t):
-    pdfObj = open('docs/sumario.pdf', 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfObj)
-    pdfPage = pdfReader.getPage(0)
-    text = pdfPage.extractText()
-    date_index = text.find('Fecha de Impresión:')
-    text_date = text[date_index+20:65]
-
-    month = text_date[0:3]
-    day = text_date[4:6]
-    year = text_date[8:12]
-    hour = text_date[18]
-    
-    time_set = text_date[26:28]
-
-    time_stamped = month+'-'+day+'-'+year+'-'+hour+time_set
-    file_name = time_stamped + '.pdf'
+def upload_to_sharepoint(status):
+    # Setting the name of the file 
+    file_name = "0" + str(date.today().day-1)+ "-" + "0" + str(date.today().day) + '_' + "Resumen" + str(date.today().year) + str(date.today().month) + '.pdf'
     path_to_file = 'docs/sumario.pdf'
-    #print('Subiendo a sharepoing..')
-    SharePoint().upload_file(path_to_file, file_name,'Sumario_PetroEcuador')
-    print(t)
+    SharePoint().upload_file(path_to_file, file_name,month_name())
+    print("Document in this sharepoint location: " + month_name() + '/' + file_name)
+
+
 
 def main():
-    schedule.every().day.at("12:15").do(get_sumary,'Descargando documento')
-    schedule.every().day.at("12:16").do(upload_to_sharepoint,'Documento en Sharepoint')
+    schedule.every().day.at("11:30").do(get_summary,'Sumario descargado')
+    schedule.every().day.at("11:31").do(upload_to_sharepoint,'Documento en Sharepoint')
 
+    # Loop
     while True:
         schedule.run_pending()
-        #print('Im Working!')
         time.sleep(1) # wait one minute
 
     
 
 if __name__ == '__main__':
-    main()
+   main()
 
 
 
