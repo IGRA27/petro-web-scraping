565f9a58244e5dbbfa798b6a93b5f5d7b0f2d792
isaac reyes <isaac.reyes@epn.edu.ec>
2023-08-05 11:48:33 -0500

    Cambio

--- main.py
+++ main.py
@@ -79,11 +79,11 @@ def upload_to_sharepoint(text):
 def main():
 
     
-    get_summary("")  # Se ejecuta inmediatamente al iniciar el script
-    upload_to_sharepoint("")  # Se ejecuta inmediatamente después de get_summary
+    #get_summary("")  # Se ejecuta inmediatamente al iniciar el script
+    #upload_to_sharepoint("")  # Se ejecuta inmediatamente después de get_summary
 
-    schedule.every().day.at("07:15:30").do(get_summary,"")
-    schedule.every().day.at("07:15:50").do(upload_to_sharepoint,"")
+    schedule.every().day.at("11:55:30").do(get_summary,"")
+    schedule.every().day.at("11:55:50").do(upload_to_sharepoint,"")
 
     # Loop
     while True:
