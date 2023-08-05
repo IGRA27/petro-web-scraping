40b11725116e2f42a38d8aff6a3cff938cdd6df6
isaac reyes <isaac.reyes@epn.edu.ec>
2023-08-05 10:17:06 -0500

    Add direct function execution for testing

--- main.py
+++ main.py
@@ -77,14 +77,27 @@ def upload_to_sharepoint(text):
 
 
 def main():
-   
+
+    
+    get_summary("")  # Se ejecuta inmediatamente al iniciar el script
+    upload_to_sharepoint("")  # Se ejecuta inmediatamente después de get_summary
+
     schedule.every().day.at("07:15:30").do(get_summary,"")
     schedule.every().day.at("07:15:50").do(upload_to_sharepoint,"")
 
     # Loop
     while True:
         schedule.run_pending()
-        time.sleep(1) # wait one minute
+        time.sleep(1)  # wait one minute
+
+   
+    #schedule.every().day.at("07:15:30").do(get_summary,"")
+    schedule.every().day.at("07:15:50").do(upload_to_sharepoint,"")
+
+    # Loop
+    #while True:
+    #    schedule.run_pending()
+    #    time.sleep(1) # wait one minute
     
 
 
